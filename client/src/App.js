0f181f637f6f7f1773f73cead19876f2ddaf6f2f
Jeovanni <jeovannildg@gmail.com>
2020-06-03 21:45:28 -0600

    created routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
 
@@ -28,7 +31,20 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Router>
+          <Route path='/' render={props => {
+            // const {props} = props;
+            return <div><MovieList props = {props}/></div>
+          }}/>
+          <Route path='/movies/:id' render={props => {
+            const {id} = props.match.params;
+            return <div><Movie props = {id}/></div>
+          }}/>
+
+          <Link to='/movies'>Movies</Link>
+        </Router>
+      </div>
     </div>
   );
 };
